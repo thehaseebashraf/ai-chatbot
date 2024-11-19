5455cff1f2f9b6e1fc4ceea7d8274cd54c45e545
thehaseebashraf <haseebashraf404@gmail.com>
2024-11-19 12:55:13 +0500

    document upload api added

--- chatbot_api/models.py
+++ chatbot_api/models.py
@@ -8,3 +8,11 @@ class ChatMessage(models.Model):
 
     def __str__(self):
         return f"Chat {self.id} - {self.created_at}"
+
+class DocumentFile(models.Model):
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    is_processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Document {self.id} - {self.uploaded_at}"
--- chatbot_api/urls.py
+++ chatbot_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ChatbotView
+from .views import ChatbotView, DocumentUploadView, DebugContextView
 
 urlpatterns = [
     path('chat/', ChatbotView.as_view(), name='chat'),
+    path('upload/', DocumentUploadView.as_view(), name='document-upload'),
+    path('debug-context/', DebugContextView.as_view(), name='debug-context'),
 ]
